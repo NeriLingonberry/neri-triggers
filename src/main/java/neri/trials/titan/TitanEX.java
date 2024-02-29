3bdebfa2dd898023bf998c0bd9e3dea1dcdd34c6
Neri Berry <Nimos438@Gmail.com>
2024-02-29 15:11:27 +0900

    Update TitanEX.java

--- src/main/java/neri/trials/titan/TitanEX.java
+++ src/main/java/neri/trials/titan/TitanEX.java
@@ -78,8 +78,9 @@ public class TitanEX implements FilteredEventHandler {
 	}
 	@HandleEvents(name = "tumult")
 	public void tumult(EventContext context, AbilityUsedEvent event) {
-		if (event.getAbility().getId() == 0x5C4) {
+		if (event.getAbility().getId() == 0x5B9) {
 			context.accept(tumult.getModified(event));
+				public long refireMs = 5_000;
 		}
 	}
 	@HandleEvents(name = "geocrush")
@@ -110,6 +111,7 @@ public class TitanEX implements FilteredEventHandler {
 	public void heart(EventContext context, BuffApplied event) {
 		if (event.getBuff().getId() == 0x148) {
 			context.accept(heart.getModified(event));
+				public long refireMs = 1_000;
 		}
 	}
 	@HandleEvents(name = "earthenFury")
@@ -126,9 +128,7 @@ public class TitanEX implements FilteredEventHandler {
 	}
 	@HandleEvents(name = "bombs")
 	public void bombs(EventContext context, TargetabilityUpdate event) {
-		long targetId = event.getTarget().getId();
-		System.out.println("Target ID: " + targetId); // Add this line for debugging
-		if (targetId == 1504) {
+		if (event.getTarget().getId() == 1504) {
 			context.accept(bombs.getModified(event));
 		}
 	}
